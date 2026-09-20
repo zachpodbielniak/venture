4dd23dfa99d95e1ac567285c005422cfbc13b3a2
Ben Doty <creplav@gmail.com>
2026-09-19 20:48:45 -0400

    fix(db): combine project and integration lifecycle guards

--- src/db/venture-database.c
+++ src/db/venture-database.c
@@ -1181,7 +1181,7 @@ check_subsystem_write(VentureDatabase *self, VentureEntity *entity, gboolean rem
 		venture_sales_tax_check_write,
 		venture_backup_schedule_check_write,
 		venture_crm_import_check_write,
-		venture_integration_check_write
+		venture_integration_check_write,
 		venture_projects_check_write
 	};
 	guint i;
